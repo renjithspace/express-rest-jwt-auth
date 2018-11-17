33d81a1f636688728598db89e07d624934f534fd
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 14:01:00 +0530

    Register routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const config = require('./config')
+const routes = require('./routes')
 
 const app = express()
-
-app.get(config.endpoint, (request, response) => {
-  response.json({ env: 'live', version: '1.0.0', status: 'up' })
-})
+routes.register(app)
 
 app.listen(config.port, () => console.log(`API server running on port ${config.port}`))
