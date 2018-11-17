7b2ca8d1bef005d53ff220df53f07684e56e0a81
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 15:41:46 +0530

    Validate with users data

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,16 @@
 const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 const config = require('../config')
+const db = require('../db')
 
 module.exports = {
   post (req, res) {
     const data = req.body
+    const user = _.find(db.users, data)
+
+    if (!user) return res.status(401).send('Unauthorized')
+
     data.token = jwt.sign(req.body, config.secret)
-    
     res.json(data)
   }
 }
