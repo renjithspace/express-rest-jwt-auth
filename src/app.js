f5c3df8e8f3f577c605d7464446e8dfc6a28f604
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 13:34:54 +0530

    Port from config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const config = require('./config')
 
 const app = express()
-const port = 8080
 
 app.get('/api', (request, response) => {
   response.json({ env: 'live', version: '1.0.0', status: 'up' })
 })
 
-app.listen(port, () => console.log(`API server running on port ${port}`))
+app.listen(config.port, () => console.log(`API server running on port ${config.port}`))
