ebf34ba91594e2d45e45025e7bc3ab01bb69483f
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 17:12:15 +0530

    Add request verify middlware

--- routes.js
+++ routes.js
@@ -1,9 +1,22 @@
 const config = require('./config')
 const controllers = require('./controllers')
+const jwt = require('jsonwebtoken')
+
+function verify(req, res, next) {
+  const token = req.headers.authorization
+  const forbidden = () => res.status(403).send('Forbidden')
+
+  if (!token) return forbidden()
+
+  jwt.verify(token, config.secret, (error, data) => {
+    if (error) return forbidden()
+    next()
+  })
+}
 
 const register = app => {
   app.post(`${config.endpoint}/auth`, (req, res) => controllers.auth.post(req, res))
-  app.get(`${config.endpoint}/books`, (req, res) => controllers.books.get(req, res))
+  app.get(`${config.endpoint}/books`, verify, (req, res) => controllers.books.get(req, res))
 }
 
 module.exports = { register }
