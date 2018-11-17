1d431fc14e8ed73a463b6d35fb6ee5ee8faa0f78
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 14:54:44 +0530

    Add auth router

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const config = require('./config')
 const controllers = require('./controllers')
 
 const register = app => {
+  app.get(`${config.endpoint}/auth`, (req, res) => controllers.auth.get(req, res))
   app.get(`${config.endpoint}/books`, (req, res) => controllers.books.get(req, res))
 }
 
