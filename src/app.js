4b4321fd3f8ffb08765849c5f95d1154ac3820d5
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 15:23:24 +0530

    Init body parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const config = require('./config')
 const routes = require('./routes')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 routes.register(app)
 
 app.listen(config.port, () => console.log(`API server running on port ${config.port}`))
