41f2a8be1deef4fe313d6b059d419c2df4802dd1
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 17:23:00 +0530

    Body parse middleware from module

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express')
-var bodyParser = require('body-parser')
 const config = require('./config')
 const routes = require('./routes')
+const middleware = require('./middleware')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
+middleware.bodyParse(app)
 routes.register(app)
 
 app.listen(config.port, () => console.log(`API server running on port ${config.port}`))
