36cba92169577ea319a1a6ac1c4302666b51a454
renjithspace <renjithrajeevvk@gmail.com>
2018-11-17 17:17:16 +0530

    Verify middleware to module

--- routes.js
+++ routes.js
@@ -1,22 +1,10 @@
 const config = require('./config')
 const controllers = require('./controllers')
-const jwt = require('jsonwebtoken')
-
-function verify(req, res, next) {
-  const token = req.headers.authorization
-  const forbidden = () => res.status(403).send('Forbidden')
-
-  if (!token) return forbidden()
-
-  jwt.verify(token, config.secret, (error, data) => {
-    if (error) return forbidden()
-    next()
-  })
-}
+const middleware = require('./middleware')
 
 const register = app => {
   app.post(`${config.endpoint}/auth`, (req, res) => controllers.auth.post(req, res))
-  app.get(`${config.endpoint}/books`, verify, (req, res) => controllers.books.get(req, res))
+  app.get(`${config.endpoint}/books`, middleware.verify, (req, res) => controllers.books.get(req, res))
 }
 
 module.exports = { register }
